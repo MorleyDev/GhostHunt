fe61206cb4b65ac3773c81991dfa7173617e172c
MorleyDev <jason@morleydev.co.uk>
2014-04-10 23:05:33 +0000

    Fixed buildscript for compiled dependencies

--- buildscripts/download_compiled_dependencies.sh
+++ buildscripts/download_compiled_dependencies.sh
@@ -1,13 +1,18 @@
+mkdir -p ext/include
+mkdir -p ext/lib
+mkdir -p ext/bin
+
 git clone https://github.com/MorleyDev/Emna emna_source --depth 1
 mkdir -p emna_build
 pushd emna_build
-	./../emna_source/buildscripts/download_header_only_dependencies.sh
+	../emna_source/buildscripts/download_header_only_dependencies.sh
 	cmake ../emna_source -DCMAKE_BUILD_TYPE=$1 -DDISABLE_SDL=ON -DDISABLE_SFML=ON
 	cmake --build .
 popd
 
-cp ./emna_source/include/* ./ext/include/ -r
-cp ./emna_build/src/*.so* ./ext/bin/ -r
+cp ./emna_source/src/Emna ./ext/include/ -r
+cp ./emna_build/src/*.so* ./ext/lib/ -r
 
 rm emna_source -rf
 rm emna_build -rf
+
