9adee209db021c48a4344640a2e91fb9c7c66b50
MorleyDev <jason@morleydev.co.uk>
2014-05-15 12:18:20 +0100

    Now only launches single instance of game per run

--- src/main/scala/uk/co/morleydev/ghosthunt/Main.scala
+++ src/main/scala/uk/co/morleydev/ghosthunt/Main.scala
@@ -4,17 +4,11 @@ import uk.co.morleydev.ghosthunt.model.Configuration
 
 object Main {
 
-  def main(args : Array[String]) {
+  def main(args: Array[String]) {
     val config = Configuration.loadOrWrite("config.json")
 
-    val threads = Iterator.continually(new Thread(new Runnable {
-      override def run(): Unit = {
-        val game = new Game(config)
-        game.run()
-      }
-    })).take(3)
+    val game = new Game(config)
+    game.run()
 
-    threads.foreach(_.start())
-    threads.foreach(_.join())
   }
 }
